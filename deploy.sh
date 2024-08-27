66a8059620ec5c8533d9b3d34062628fa25b92fc
Renan LE CARO <renan.lecaro@gmail.com>
2024-08-27 10:46:23 +0200

    Demo site

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DOMAIN="nanosheets.lecaro.me"
-ssh root@128.140.45.246 "mkdir -p /opt/mup-nginx-proxy/config/html/static_sites/$DOMAIN"
-rsync -avz --delete --delete-excluded --exclude="*.sh" --exclude="node_modules" --exclude=".*"  . root@128.140.45.246:/opt/mup-nginx-proxy/config/html/static_sites/$DOMAIN
+ssh staging "mkdir -p /opt/mup-nginx-proxy/config/html/static_sites/$DOMAIN"
+rsync -avz --delete --delete-excluded --exclude="*.sh" --exclude="node_modules" --exclude=".*"  . staging:/opt/mup-nginx-proxy/config/html/static_sites/$DOMAIN
